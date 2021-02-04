12f591e2360ec56dd1bba1fb1bd45d29eb52f099
licup <edvlicup@gmail.com>
2021-02-04 12:49:56 -0800

    put classes for Search Screen

---
+++ app/src/main/java/com/example/cst438_proj1/SearchJobs.java
@@ -0,0 +1,12 @@
+package com.example.cst438_proj1;
+
+public class SearchJobs {
+    private String keyword;
+    private String location;
+    private int radius;
+
+    /**
+     * Type in Search bar
+     * switch to a view that shows results?
+     */
+}
---
+++ app/src/main/java/com/example/cst438_proj1/SearchResults.java
@@ -0,0 +1,7 @@
+package com.example.cst438_proj1;
+
+public class SearchResults {
+    /**
+     * Display jobs depending on criteria put in SearchJobs
+     */
+}
