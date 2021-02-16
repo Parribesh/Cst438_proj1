c0a1653743d4d71f0455c8a32f540f22f526c2c9
licup <edvlicup@gmail.com>
2021-02-15 23:43:32 -0800

    Set up SearchJobs, need to use API still

--- app/src/main/java/com/example/cst438_proj1/SearchResults.java
+++ app/src/main/java/com/example/cst438_proj1/SearchResults.java
@@ -1,7 +1,9 @@
 package com.example.cst438_proj1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,12 +15,24 @@ public class SearchResults extends AppCompatActivity {
      *
      */
     private TextView results;
-
+    private TextView topMsg;
+    private Button backBtn;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_search_results);
 
+        results = findViewById(R.id.result);
+        topMsg = findViewById(R.id.welcomeMsg);
+        backBtn = findViewById(R.id.backBtn);
+
+        topMsg.setText("Results:");
+        topMsg.setTextSize(20);
+
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(SearchResults.this, SearchJobs.class);
+            startActivity(intent);
+        });
     }
 }
